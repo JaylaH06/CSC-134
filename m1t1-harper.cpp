48069b11338a50c08a31128add142c7f38b5561b
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-01-14 16:01:53 +0000

    added more cout statements

--- m1t1-harper.cpp
+++ m1t1-harper.cpp
@@ -7,7 +7,15 @@
 using namespace std;
 
 int main() {
+    cout << "I want cheezburger" <<endl;
     cout << "Hello, New York!" << endl; // E N D L
     cout << endl; // another new line
+
+    /*
+    nest we will:
+    - make a variable (a noun) to store the user's name
+    - ask them for the name
+    - greet them by name
+    */
     return 0; // no errors
 }
