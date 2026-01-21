1a3516c090dd0fcaec850e50c4e4eeb15ea64e41
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-01-21 16:39:25 +0000

    Working on M1HW1

---
+++ M1HW1_harper.cpp
@@ -0,0 +1,20 @@
+// CSC 13
+// M1HW1- Movie Talk
+// 01/21/26
+// Jayla Harper
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    string movie_name = "Ponyo";
+    int movie_year = 2008;
+    double movie_gross = 206.5;
+
+    cout << setprecision(2) << fixed;
+    cout << My favorite quote from the movie is "I'll always love Ponyo whether she's a fish, a human... or in between"
+    cout << My favorite scene from the movie is when Ponyo runs towards Sosuke to hug him on the road
+
+
+}
