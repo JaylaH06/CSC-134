94e35a4f59709015d88575d3873fcbad46bbe589
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-01-21 16:09:10 +0000

    Worked on  M1Lab1

--- M1Lab_harper.cpp
+++ M1Lab_harper.cpp
@@ -1,5 +1,5 @@
 // CSC 134
-// M1Lab1
+// M1Lab1 - We're sellinh something
 // 01/21/26
 // Jayla Harper
 
@@ -7,5 +7,22 @@
 using namespace std;
 
 int main() {
+    // Declare my variables
+    string item_name = "books";
+    int item_count =2000;
+    double item_cost = 1.39;
+    double total_cost;
+
+
+
+    cout << "Welcome to the " << item_name << " store." << endl;
+    cout << "We have " << item_count << " " << item_name << "." << endl;
+    cout << "They cost $" << item_cost << " each." << endl;
+    total_cost = item_count * item_cost;
+
+
+
+    cout << "To buy all of them will cost $" << total_cost << endl;
+
     return 0;
 }
