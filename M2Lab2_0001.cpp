fb43c371f4786e5466a480b3381b321d57de78c8
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-01-28 15:57:45 +0000

    Added Inputs to M2Lab2

--- M2Lab2_0001.cpp
+++ M2Lab2_0001.cpp
@@ -11,6 +11,7 @@ Print everything out
 
 #include <iostream>
 #include <iomanip>
+using namespace std;
 
 int main() {
     // Part 1 -- Declare Variables
@@ -24,10 +25,19 @@ int main() {
     double cost, customer_price, profit; // all in $
 
     // Part 2 -- Get the Input
+    cout << "Welcome to the Crate Program." << endl;
+    cout << "Enter the crate dimensions." << endl;
+
+    cout << "Crate Length";
+    cin >> length;
+    cout << "Crate Width";
+    cin >> width;
+    cout << "Crate Height";
+    cin>> height;
 
     // Part 3 --  Do the Calculations
 
-    // Part4 -- Print the Output
+    // Part 4 -- Print the Output
     
     return 0;
 }
