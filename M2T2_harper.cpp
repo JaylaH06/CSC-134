cb8224eed528700b109f923f4d4f33d0fffc71d0
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-01-29 00:41:56 +0000

    Set Variables

--- M2T2_harper.cpp
+++ M2T2_harper.cpp
@@ -7,6 +7,21 @@
 using namespace std;
 
 int main() {
+    // Declare all variables
+    string meal_name = "Salmon Platter";
+    double meal_price = 5.99;
+    double tax_rate = 0.08;
+    double tip_amount;
+    double tax_amount;
+    double total_price;
+
+    // Get user input
+
+
+    // Do the calculations
+
+
+    // Present the output
 
     return 0;
 }
