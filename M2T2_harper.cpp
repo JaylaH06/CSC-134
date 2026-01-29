84c70ec2b66ca9c9e28d61497a93bbd2bb90b83a
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-01-29 01:08:17 +0000

    Basic output

--- M2T2_harper.cpp
+++ M2T2_harper.cpp
@@ -9,19 +9,29 @@ using namespace std;
 int main() {
     // Declare all variables
     string meal_name = "Salmon Platter";
+    int num_meals;
     double meal_price = 5.99;
+    double sub_total;
     double tax_rate = 0.08;
     double tip_amount;
     double tax_amount;
     double total_price;
 
     // Get user input
-
+    cout << "Welcome to the CSC 134 Grill." << endl;
+    cout << "Today's Special: " << meal_name << endl;
+    cout << endl;
+    cout << "How many would you like? ";
+    cin >> num_meals;
 
     // Do the calculations
-
+    sub_total = meal_price * num_meals;
 
     // Present the output
+    cout << endl;
+    cout << "YOUR ORDER" << endl << "--------------------" << endl;
+    cout << num_meals << " x " << meal_name << "\t$" << meal_price << endl;
+    cout << "Subtotal: \t$" << sub_total << endl;
 
     return 0;
 }
