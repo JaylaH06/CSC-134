b7eae3e76dba3c37012b68f432d50fecec567f73
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-02-11 06:48:52 +0000

    Added some outputs and inputs

--- M2HW1_harper.cpp
+++ M2HW1_harper.cpp
@@ -11,9 +11,27 @@ using namespace std;
 
 int main() {
 
+    double start_balance;
+    double deposit_amount;
+    double withdrawl_amount;
+
     cout << "Welcome to The Diamond Galore Bank." << endl;
     string name;
     cout << "What's your User? ";
     cin >> name;
     cout << "Welcome back, " << name << "!" << " Happy to see you using our services again. What can we do for you? " << endl;
+    cout << endl;
+    cout << "What is the Starting Account Balance? ";
+    cin >> start_balance;
+    cout << "What is the amount that you wish to deposit? ";
+    cin >> deposit_amount;
+    cout << "What is the amount you wish to withdraw? ";
+    cin >> withdrawl_amount;
+
+
+
+
+
+
+    return 0;
 }
