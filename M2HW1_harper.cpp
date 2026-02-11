5c2deebdad3503681aac2ca2d3db30e99d958692
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-02-11 06:15:25 +0000

    Starting M2HW1

---
+++ M2HW1_harper.cpp
@@ -0,0 +1,19 @@
+/*
+CSC 134
+M2HW1 - Gold Grading Tiers
+Jayla Harper
+2/11/26
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    cout << "Welcome to The Diamond Galore Bank." << endl;
+    string name;
+    cout << "What's your User? ";
+    cin >> name;
+    cout << "Welcome back, " << name << "!" << " Happy to see you using our services again. What can we do for you? " << endl;
+}
