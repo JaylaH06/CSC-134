492d599a103310a2b5e175546cf3c9ad4fe63c3e
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-02-23 15:51:55 +0000

    List the choices

--- M3Lab1_harper.cpp
+++ M3Lab1_harper.cpp
@@ -6,3 +6,16 @@
 #include <iostream>
 using namespace std;
 
+// List prtotypes of all choices up here
+void Bubbly_woowoo_1();
+void Sparkly_Wallbanger_2();
+void Fuzzy_Navel_3();
+void snake_wine_4();
+
+// main() goes here
+int main() {
+
+    int choice
+}
+
+// List full functions of all the choices at  the bottom
