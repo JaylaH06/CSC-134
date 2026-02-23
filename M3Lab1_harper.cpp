5da3fa52296c89ea4cf1404fea014a799b696e97
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-02-23 16:34:19 +0000

    Adding the inputs

--- M3Lab1_harper.cpp
+++ M3Lab1_harper.cpp
@@ -10,12 +10,69 @@ using namespace std;
 void Bubbly_woowoo_1();
 void Sparkly_Wallbanger_2();
 void Fuzzy_Navel_3();
-void snake_wine_4();
+void Snake_wine_4();
 
 // main() goes here
 int main() {
 
-    int choice
+    int choice;
+    // main() starts the game
+    cout << "Spell-Check The Brewing Game 🧙" << endl << endl;
+    cout << " You're a lonesome witch that lives in the woods. A 'Coven of One' if you will.";
+    cout << " But like any witch you need to make a living, newt tails aren't cheap.";
+    cout << " So you make potions for the creatures of the forest to fund your lifestyle.";
+    cout << " Todays customer is an Nidhogg who wantss to visit his grandma in the Netherlands.";
+    cout << " The only problem is that Mr. Nidhogg has horrible seasonal allergies and needs something to make the journey there a bit easier on his sinuses.";
+    cout << " I have everything else set up. But for somme reason I can't remember the last ingredient.";
+    cout << " So, I need and I need your help!";
+    cout << " There's four different bottles here which one should we use?";
+
+    cout << "Do you: " << endl;
+    cout << "1. Bubbly WooWoo" << endl;
+    cout << "2. Sparkly WallBanger" << endl;
+    cout << "3. Fuzzy Navel" << endl;
+    cout << "4. Snake Wine" << endl;
+
+    cout << "> "; // Give them a prompt to type
+    cin >> choice;
+
+    if (1 == choice) {
+        Bubbly_woowoo_1();
+    }
+    else if (2 == choice) {
+        Sparkly_Wallbanger_2();
+    }
+    else if (3 == choice) {
+         Fuzzy_Navel_3();
+    }
+    else if (4 == choice) {
+        Snake_wine_4();
+    }
+    else {
+        cout << "We couldn't pick up that ingredient...They raised the prices of Spider Eyes." << endl;
+    }
+    cout << "*Game Over*" << endl << endl;
+
+    return 0; // End of Game
 }
 
 // List full functions of all the choices at  the bottom
+void Bubbly_woowoo_1() {
+    cout << " AHA Lets go with the first potion then!" << endl;
+    cout << " We mix in the potion and give it to Mr. Nidhogg..."; // You finissh it!
+
+}
+void Sparkly_Wallbanger_2() {
+    cout << " AHA Lets go with the second potion then!" << endl;
+    cout << " We mix in the potion and give it to Mr. Nidhogg...";
+
+}
+void Fuzzy_Navel_3() {
+    cout << " AHA Lets go with the third potion then!" << endl;
+    cout << " We mix in the potion and give it to Mr. Nidhogg...";  
+
+}
+void Snake_wine_4() {
+    cout << " AHA Lets go with the fourth potion then!" << endl;
+    cout << " We mix in the potion and give it to Mr. Nidhogg...";
+}
