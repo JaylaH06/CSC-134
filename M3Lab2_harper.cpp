4db8567982407bceb031d86bc4f1fdac942cebbe
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-02-25 15:34:39 +0000

    Started M3Lab2

---
+++ M3Lab2_harper.cpp
@@ -0,0 +1,24 @@
+/*
+CSC 134
+M3Lab2- Number Grade to Letter Grade
+Jayla Harper
+2/25/26
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num_grade;
+    string letter_grade;
+    cout << " This program takes a number grade (0-100) and provides the letter grade (A-F)" << endl;
+    // Input
+    cout << " Enter number grade: ";
+    cin >> num_grade;
+    // calculate
+
+    // Output
+    cout << " A grade of " << num_grade << " is letter grade: " << letter_grade << endl;
+
+    return 0;
+}
