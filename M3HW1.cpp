0890bc245e6f1c2d590c65d37ed67941fc753baa
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-03-09 01:41:46 +0000

    Finished Question 3

--- M3HW1.cpp
+++ M3HW1.cpp
@@ -17,7 +17,7 @@ int main() {
     string input;
 
     cout << "Hello, I'm a C++ program!";
-    cout << "Do you like me?";
+    cout << " Do you like me?";
     cout << "\nPlease type yes or no: ";
     getline(cin, input);
     
@@ -32,47 +32,187 @@ int main() {
     // Question 2
     cout << "Question 2" << endl;
 
-     string meal_name = "Salmon Platter";
+    // Declare all variables
+    string meal_name = "Salmon Platter";
     int num_meals;
-    double meal_price = 5.99;
+    double meal_price;
     double sub_total;
     double tax_rate = 0.08;
-    double tip_amount;
+    double tip_amount = 0.0;
+    double tip_rate = 0.15;
     double tax_amount;
     double total_price;
+    int order_type;
 
     // Get user input
     cout << "Welcome to the CSC 134 Grill." << endl;
     cout << "Today's Special: " << meal_name << endl;
     cout << endl;
-    cout << "What is the price of this meal?" << endl;
-    getline(cin, meal_price);
 
-    if (meal_price == 5.99) {
-        cout << "Perfect! Is this order Dine in or take away?" << endl;
-    }   else {
-        cout << "I'm not sure if this is the right price?" << endl;
-    }
+    cout << "What is the price of the meal? $";
+    cin >> meal_price;
+
     cout << "How many would you like? ";
     cin >> num_meals;
-    cout << "Tip amount? (min 0)? ";
-    cin >> tip_amount;
+
+    cout << "Please enter 1 if the order is dine in, 2 if it is to go: ";
+    cin >> order_type;
 
     // Do the calculations
     sub_total = meal_price * num_meals;
     tax_amount = sub_total * tax_rate;
+
+    if (order_type == 1) {
+        tip_amount = sub_total * tip_rate;
+    }
+
     total_price = sub_total + tip_amount + tax_amount;
 
     // Present the output
     cout << setprecision(2) << fixed;
     cout << endl;
-    cout << "YOUR ORDER" << endl << "--------------------" << endl;
+    cout << "YOUR ORDER" << endl;
+    cout << "--------------------" << endl;
     cout << num_meals << " x " << meal_name << "\t$" << meal_price << endl;
     cout << "Subtotal: \t\t$" << sub_total << endl;
-    cout << "Tip: \t\t$" << tip_amount << endl;
-    cout << "Tax: \t\t$" << tax_amount << endl;
+    cout << "Tax (7%): \t\t$" << tax_amount << endl;
+
+    if (order_type == 1) {
+        cout << "Tip (15%): \t\t$" << tip_amount << endl;
+    }
+
     cout << "--------------------" << endl;
-    cout << "Total: \t\t$" << total_price << endl;
+    cout << "Order Type: \t\t" << (order_type == 1 ? "Dine In" : "To Go") << endl;
+    cout << "Total: \t\t\t$" << total_price << endl;
     cout << "THANK YOU COME AGAIN <3" << endl;
+
+    // Question 3
+    cout << "Quetion 3" << endl;
+
+    string name;
+    int choice;
+    int gryffindor = 0, slytherin = 0, ravenclaw = 0, hufflepuff = 0;
+
+    cout << "========================================" << endl;
+    cout << "   Welcome to the Hogwarts Sorting Quiz  " << endl;
+    cout << "========================================" << endl;
+    cout << "What is your name? ";
+    cin >> name;
+    cout << endl;
+    cout << "Hello, " << name << "! The Sorting Hat will now ask you" << endl;
+    cout << "5 questions to determine your house." << endl;
+    cout << endl;
+
+    cout << "Question 1: What do you value most?" << endl;
+    cout << "1. Courage and Bravery" << endl;
+    cout << "2. Ambition and Cunning" << endl;
+    cout << "3. Wisdom and Knowledge" << endl;
+    cout << "4. Loyalty and Hardwork" << endl;
+    cout << "Your choice (1-4): ";
+    cin >> choice;
+    
+    if (choice == 1) gryffindor++;
+    else if (choice == 2) slytherin++;
+    else if (choice == 3) ravenclaw++;
+    else if (choice == 4) hufflepuff++;
+    cout << endl;
+
+    cout << "Question 2: You find a wallet full of money on the ground." << endl;
+    cout << " What do you do?" << endl;
+    cout << "1. Turn it in immediately -- it's the right thing to do!" << endl;
+    cout << "2. Keep it, Finders keepers." << endl;
+    cout << "3. Research the best way to find the owner." << endl;
+    cout << "4. Ask around and try to return it to someone nearby." << endl;
+    cout << "Your choice (1-4): ";
+    cin >> choice;
+    if (choice == 1) gryffindor++;
+    else if (choice == 2) slytherin++;
+    else if (choice == 3) ravenclaw++;
+    else if (choice == 4) hufflepuff++;
+    cout << endl;
+
+    cout << "Question 3: Which class would you enjoy most at Hogwarts?" << endl;
+    cout << "1. Defense against The Dark Arts." << endl;
+    cout << "2. Potions" << endl;
+    cout << "3. Charms or Arthimancy" << endl;
+    cout << "4. Herbology or Care of Magical Creatures." << endl;
+    cout << "Your choice (1-4): ";
+    cin >> choice;
+    if (choice == 1) gryffindor++;
+    else if (choice == 2) slytherin++;
+    else if (choice == 3) ravenclaw++;
+    else if (choice == 4) hufflepuff++;
+    cout << endl;
+
+    cout << "Question 4: How do your friends describe you?" << endl;
+    cout << "1. Bold and Daring" << endl;
+    cout << "2. Determined and Resourceful" << endl;
+    cout << "3. Thoughtful and Curious" << endl;
+    cout << "4. Kind and Dependable" << endl;
+    cout << "Your choice (1-4): ";
+    cin >> choice;
+    if (choice == 1) gryffindor++;
+    else if (choice == 2) slytherin++;
+    else if (choice == 3) ravenclaw++;
+    else if (choice == 4) hufflepuff++;
+    cout << endl;
+
+    cout << "Question 5: What is your greatest fear?" << endl;
+    cout << "1. Being seen as a coward" << endl;
+    cout << "2. Failure or losing power" << endl;
+    cout << "3. Being seen as ignorant" << endl;
+    cout << "4. Letting down those you love" << endl;
+    cout << "Your choice (1-4): ";
+    cin >> choice;
+    if (choice == 1) gryffindor++;
+    else if (choice == 2) slytherin++;
+    else if (choice == 3) ravenclaw++;
+    else if (choice == 4) hufflepuff++;
+    cout << endl;
+
+    string house, description, members;
+    int maxScore = gryffindor;
+
+    if (slytherin > maxScore) maxScore = slytherin;
+    if (ravenclaw > maxScore)  maxScore = ravenclaw;
+    if (hufflepuff > maxScore) maxScore = hufflepuff;
+
+    if (maxScore == gryffindor) {
+        house       = "GRYFFINDOR";
+        description = "You are brave, daring, and full of nerve.\n"
+                      "You face challenges head-on and inspire others.";
+        members     = "Harry Potter, Hermione Granger, Ron Weasley";
+    } else if (maxScore == slytherin) {
+        house       = "SLYTHERIN";
+        description = "You are ambitious, shrewd, and determined.\n"
+                      "You know what you want and how to get it.";
+        members     = "Draco Malfoy, Severus Snape, Merlin";
+    } else if (maxScore == ravenclaw) {
+        house       = "RAVENCLAW";
+        description = "You are wise, creative, and love learning.\n"
+                      "Your sharp mind is your greatest asset.";
+        members     = "Luna Lovegood, Cho Chang, Filius Flitwick";
+    } else {
+        house       = "HUFFLEPUFF";
+        description = "You are loyal, patient, and hardworking.\n"
+                      "You value fairness and care deeply for others.";
+        members     = "Cedric Diggory, Nymphadora Tonks, Newt Scamander";
+}
+
+cout << "========================================" << endl;
+    cout << "   THE SORTING HAT HAS DECIDED..." << endl;
+    cout << "========================================" << endl;
+    cout << endl;
+    cout << name << ", you belong in..." << endl;
+    cout << endl;
+    cout << "*** " << house << " ***" << endl;
+    cout << endl;
+    cout << description << endl;
+    cout << endl;
+    cout << "Notable alumni: " << members << endl;
+    cout << endl;
+    cout << "========================================" << endl;
+    cout << "  Welcome to your new home, " << name << "!" << endl;
+    cout << "========================================" << endl;
 
 }
