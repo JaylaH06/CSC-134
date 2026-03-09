4b8f7c488c7de1464c5a316e90cf68f32994d99e
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-03-09 15:36:26 +0000

    Working on M4HW1

--- M4HW1_harper.cpp
+++ M4HW1_harper.cpp
@@ -12,22 +12,30 @@ int main() {
 
     // Declare variables
     int firstNum, secondNum, answer, sum;
-    firstNum = 3;
+    //firstNum = 3;
     //secondNum = 7;
     //sum = firstNum * secondNum;
 
     // A sample message, with variables
     // cout << "2 times 1 is 2\n";
+
+     cout << "Enter a number to multiply: " << endl;
     for (int i=1; i <= 5; i++) {
-        sum = firstNum * i;
-        cout << "What is " << firstNum << " times " << i << "\n";
+    for (int j=1; j <= 5; j++) {
+          cin >> i;
+        sum = i * j;
+
+    cout << "What is " << i << " times " << j << "\n";
     cin >> answer;
     if (answer == sum) {
         cout << "Great Job!" << endl;
     } else {
         cout << "Uhh that isn't right" << "\n" << "It's actually " << sum << endl;
     }
-    cout << firstNum << " times " << i << " is " << sum << endl;
+    cout << i << " times " << j << " is " << sum << endl;
     }
+    cout << endl;
+    }
+      
     
 }
