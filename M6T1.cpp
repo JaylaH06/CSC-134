654a31228662a2c8b734b4597f72cf759857016a
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-04-13 15:03:20 +0000

    Working on M6T1

---
+++ M6T1.cpp
@@ -0,0 +1,32 @@
+/*
+CSC 134
+Jayla Harper
+M6T1 Basic Arrays
+04/13/26
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // set up variables
+    const int SIZE =5; // chaning this is the only way to resize the array
+    int cars[SIZE]; // contains 5 (SIZE) intergers
+    double sum = 0;
+    double average;
+    cout << "This program will ask you to enter a count of cars seen." << endl;
+    // first loop: gget data
+    for (int i=0; i<SIZE; i++) {
+        cout << "Day " << i+1 << ": ";
+        cin >> cars[i];
+    }
+    // second loop: sum and average
+    for (int i=0; i<SIZE; i++) {
+        sum += cars[i]; // add that day's cars
+    }
+    average = sum / SIZE;
+    cout << "Over " << SIZE << " days," << endl;
+    cout << "Total cars: " << sum << endl;
+    cout << "Average:   " << average << endl;
+}
