b091768bf5c0fca75fd0add9820596de204625f4
JaylaH06 <harperj3153@student.faytechcc.edu>
2026-04-20 15:27:25 +0000

    Starting the definition connections

--- M6Lab2.cpp
+++ M6Lab2.cpp
@@ -51,12 +51,24 @@ int main() {
 
     // Room descriptions array
     string roomDescriptions[NUM_ROOMS] = {
-        "A dark and damp basement. You can hear the sound of rats surrying.",
-        "A kitchen with the putrid smell of old meat.",
-        "A rather dull looking room with a fireplace... Are those bones inside the fireplace?",
-        "A spacious master bedroom with a king-sized bed.",
-        "A clean bathroom with a shower.",
-        "A dusty attic filled with old boxes.",
-        "The front door leading outside."
+        "A dark and damp basement. You can hear the sound of rats scurrying. Your head feels heavy and you can't remeber how you got here. You just know that you need to get out.",
+        "A kitchen with the putrid smell of raw meat. The counters are covered in blood and there are strange symbols on the walls.",
+        "A rather dull looking room with a fireplace... And is that bones inside the fireplace?",
+        "A spacious master bedroom with a king-sized bed. Pictures of smiling people are hung up all over the walls.There's a closet in the corner but it's locked.",
+        "A relatively clean bathroom with a shower. There's a multitude of cleaning supplies on the shelves, none that you recognize but they should come in handy.",
+        "A dusty attic filled with old boxes. Cobwebs cover the corner's and the floor creaks with every step. You can see a faint light coming from one of the boxes.",
+        "The front door leading outside. It's bolted shut with three heavy duty locks and a chain. You can see sunlight peeking through the crracks in the door."
     };
+
+
+    // Adjacency list using a 2D array
+    // Connections[from_room][direction] = to_room or -1 if no connections
+    int connections[NUM_ROOMS][NUM_DIRECTIONS];
+
+    // Initialize all connections to -1 (no connection)
+    for (int i = 0; i < NUM_ROOMS; i++) {
+        for (int j = 0; j < NUM_DIRECTIONS; j++) {
+            connections[i][j] = -1;
+        }
+    }
 }
